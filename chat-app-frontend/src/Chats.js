36dc613c998e6a1ef72429cbe55cda0cd144cb0f
codershona <falgunimst95@gmail.com>
2022-11-20 16:05:32 +0000

    styling with material ui and logic and functions building

--- chat-app-frontend/src/Chats.js
+++ chat-app-frontend/src/Chats.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import './main.css';
 
-const Chats = (props) => {
+import { Card, CardContent, Typography } from '@mui/material';
+import React, { forwardRef } from 'react';
+
+const Chats = forwardRef(({ postMessage, userAccount }, ref) => {
+  var isUserAccount = userAccount === postMessage.userAccount;
   return (
-    <div>
-      <h2>
-        {props.text}
-      </h2>
+    <div className={`postMessage ${isUserAccount && 'postMessage_user'}`} ref={ref}>
+      <Card className={isUserAccount ? "chatRoom_Card" : "chatRoom_guestCard"}>
+          <CardContent>
+            <Typography 
+             sx={{ fontSize: 25 }} 
+             variant='h5'
+             component='h2'
+             color="text.secondary" 
+             className="chat_text"
+             gutterBottom>
+              {!isUserAccount && `${postMessage.userAccount || 'Anonymous User'}: speaks to `}: {postMessage.postsMessage}
+            </Typography>
+          </CardContent>
+       </Card>
     </div>
   )
-}
+})
 
-export default Chats
+export default Chats;
---
+++ chat-app-frontend/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+const firebaseApp = firebase.initializeApp({
+    // your Firebase credentials go here
+  apiKey: "AIzaSyDIF6oCEKOLY3rxyAjiTJKtUpqi7Z69S-Y",
+  authDomain: "chat-app-f-1105c.firebaseapp.com",
+  projectId: "chat-app-f-1105c",
+  storageBucket: "chat-app-f-1105c.appspot.com",
+  messagingSenderId: "83938649539",
+  appId: "1:83938649539:web:6233be4bffea2643f38976",
+  measurementId: "G-R6QGD6TG0X"
+})
+
+const database = firebaseApp.firestore()
+
+export default database;
